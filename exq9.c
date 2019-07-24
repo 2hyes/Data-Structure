7ee07955e132999a4c9174d40259af8d0630dadc
2hyes <2hyes.com@gmail.com>
2019-07-24 19:30:34 +0900

    01-02 exercise Q9

---
+++ exq9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void sumof(int, int);
+
+int main(void) {
+	int start, end;
+	printf("a와 b의 값: ");
+	scanf("%d %d", &start, &end);
+
+	sumof(start, end);
+	return 0;
+}
+void sumof(int a, int b) {
+	int i;
+	int sum = 0;
+	if (a > b) {
+		for (i = a; i >= b ; i--)
+			sum += i;
+	}
+	else {
+		for (i = a; i <= b; i++)
+			sum += i;
+	}
+	
+	printf("%d부터 %d까지의 합은 %d.\n", a, b, sum);
+}
