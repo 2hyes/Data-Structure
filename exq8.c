7d78d43db74ac32ab3ab1ee405820590cc054194
2hyes <2hyes.com@gmail.com>
2019-07-24 19:00:28 +0900

    01-02 exercise Q8

---
+++ exq8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int gauss(int);
+
+int main(void) {
+	int n, sum;
+	printf("n의 값: ");
+	scanf("%d", &n);
+
+	sum = gauss(n);
+	printf("1부터 %d까지의 합은 %d.\n", n, sum);
+}
+
+int gauss(int n) {
+	int half = 0;
+	int sum = 0;
+	//even
+	if (n % 2 == 0) {
+		half = n / 2;
+		sum = (1 + n) * half;
+	}
+	//odd
+	else {
+		half = n / 2 + 1;
+		sum = ((1 + n) * (half - 1)) + half;
+	}
+	return sum;
+}
